359d9ac77d3c8b418bab5797e6fd265b4bc0f659
JustANormalThing <yagyaev.s.i.22@gmail.com>
2024-02-07 10:53:03 +0300

    Test10

--- src/App.js
+++ src/App.js
@@ -8,27 +8,48 @@ function App() {
  </div>; 
 }
 
-function App1() {
-	return	<ul>
-    <li>text1</li>
-    <li>1</li>
-    <li>1</li>
-    <li>1</li>
-    <li>1</li>
-    <li>1</li>
-    <li>1</li>
-    <li>1</li>
-    <li>1</li>
-    <li>1</li>
-		</ul>;
+//function App1() {
+	//return	<ul>
+   // <li>text1</li>
+   // <li>1</li>
+   // <li>1</li>
+   // <li>1</li>
+   // <li>1</li>
+   // <li>1</li>
+   // <li>1</li>
+   // <li>1</li>
+   // <li>1</li>
+   // <li>1</li>
+	//	</ul>;
+//}
+
+//function App2() {
+	//return <ul>
+		//	<li>text1</li>
+		//	<li>text2</li>
+		//	<li>text3</li>
+		//</ul>;
+//}
+
+function App3() {
+	return  (
+    <>
+    <div> 
+      <ul>
+		  <li>text1</li>
+		  <li>text2</li>
+		  <li>text3</li>
+	    </ul>   
+    </div>
+    <div>
+    <ul>
+		<li>text1</li>
+		<li>text2</li>
+		<li>text3</li>
+	</ul>;
+ </div>
+ </>
+ );
 }
 
-function App2() {
-	return <ul>
-			<li>text1</li>
-			<li>text2</li>
-			<li>text3</li>
-		</ul>;
-}
-
-export default App2;
+export default App3;
