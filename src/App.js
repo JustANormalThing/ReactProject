17e3ed1010e364f0d9e67066391a59412023aed9
JustANormalThing <yagyaev.s.i.22@gmail.com>
2024-02-07 11:29:32 +0300

    Test19

--- src/App.js
+++ src/App.js
@@ -99,13 +99,24 @@ function App() {
   <p>{obj.surname}</p>
   </div>
 }
-*/
+
 function App() {
 	const attr = 'block';
 	
 	return <div id ={attr}>
 		text
 	</div>;
+}
+*/
+function App() {
+	const li1 = <div><li>text1</li></div>
+	const li2 = <div><li>text2</li></div>;
+	const li3 = <div><li>text3</li></div>;
+  return <main>
+  {li1}
+  {li2}
+  {li3}
+</main>
 }
 
 export default App;
