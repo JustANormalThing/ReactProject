25f4370be40187e16463c85d21407cdaaa7a5dc9
JustANormalThing <yagyaev.s.i.22@gmail.com>
2024-02-08 12:55:32 +0300

    Test24

--- src/App.js
+++ src/App.js
@@ -169,7 +169,7 @@ function App() {
 		</ul>
 	</div>;
 }
-*/
+
 
 function App() {
 	return <div>
@@ -203,5 +203,33 @@ function App() {
 	</div>;
 }
 
+function App() {
+	const num1 = 3;
+	const num2 = 2;
+	
+	return <div>
+		result: {num1 ** num2}
+	</div>;
+}
+
+
+function App() {
+	const name = 'john';
+	const surname = 'smit';
+	
+	return <div>
+		result: {name + ' ' + surname}
+		<p>result: {surname + ' ' + name}</p>
+	</div>;
+}
+
+function App() {
+	const num = 4;
+	
+	return <div>
+		result: {Math.sqrt(num)}
+	</div>;
+}
+*/
 export default App;
 
